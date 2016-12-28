27a7b1f5b448d2c238f0e6282a20bed0db1d2bfa
Sam Carlberg <sam.carlberg@gmail.com>
2016-12-28 02:51:20 -0500

    Try another way of tapping py3.5

--- .travis-scripts/osx-opencv-install.sh
+++ .travis-scripts/osx-opencv-install.sh
@@ -8,7 +8,9 @@ brew upgrade cmake
 brew install ant
 brew upgrade ant
 # TODO revert this once opencv-python has builds for Python 3.6
-brew install zoidbergwill/python/python35
+brew tap zoidbergwill/python
+brew install python35
+pip3.5 install -U pip setuptools
 brew linkapps python3
 pip3 install numpy
 pip3 install opencv-python
