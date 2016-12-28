825d8baff2d46231d9762e3b70d9284dcd29d112
Sam Carlberg <sam.carlberg@gmail.com>
2016-12-28 02:01:49 -0500

    Use custom tap for Python 3.5
    Revert this once opencv-python has builds for Python 3.6

--- .travis-scripts/osx-opencv-install.sh
+++ .travis-scripts/osx-opencv-install.sh
@@ -7,7 +7,8 @@ brew install cmake
 brew upgrade cmake
 brew install ant
 brew upgrade ant
-brew install python3
+# TODO revert this once opencv-python has builds for Python 3.6
+brew install zoidbergwill/python/python35
 brew linkapps python3
 pip3 install numpy
 pip3 install opencv-python
