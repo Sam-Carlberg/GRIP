df544a9abd7609aaea39e46b69bbcf5fdbd68bbe
Sam Carlberg <sam.carlberg@gmail.com>
2016-12-28 03:01:12 -0500

    Use pyenv

--- .travis-scripts/osx-opencv-install.sh
+++ .travis-scripts/osx-opencv-install.sh
@@ -8,9 +8,10 @@ brew upgrade cmake
 brew install ant
 brew upgrade ant
 # TODO revert this once opencv-python has builds for Python 3.6
-brew tap zoidbergwill/python
-brew install python35
-pip3.5 install -U pip setuptools
+brew install pyenv
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+exec $SHELL
+pyenv install 3.5.2
 brew linkapps python3
 pip3 install numpy
 pip3 install opencv-python
